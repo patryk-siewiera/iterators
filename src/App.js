f394f8745372245a1fba6c4768231c3ebb0df594
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-16 12:43:12 +0200

    feat: add new list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import React, { useState } from "react";
+
+function newForm(name) {
+	const form = {
+		name: name,
+	};
+	return form;
+}
 
 function App() {
-	return <div className="App">react app</div>;
+	const [formList, setFormList] = useState([]);
+	return (
+		<div className="App">
+			<h1>PS forms</h1>
+			<button
+				onClick={(e) => setFormList(formList.concat(newForm("list1")))}
+			>
+				New form
+			</button>
+			<div>{JSON.stringify(formList)}</div>
+		</div>
+	);
 }
 
 export default App;
