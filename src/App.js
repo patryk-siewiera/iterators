4ed4ae0fa5b091c97addc269496a53359becaec6
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-16 13:10:00 +0200

    feat: render list

--- src/App.js
+++ src/App.js
@@ -15,31 +15,47 @@ function App() {
 	const [desc, setDesc] = useState("");
 	return (
 		<div className="App">
-			<h1>PS forms</h1>
-			<input
-				onChange={(ev) => setName(ev.target.value)}
-				type="text"
-				placeholder="List Name"
-				id="name"
-				value={name}
-			/>
-			<input
-				onChange={(ev) => setDesc(ev.target.value)}
-				type="text"
-				placeholder="List Desciption"
-				id="description"
-				value={desc}
-			/>
-			{/* https://youtu.be/pCA4qpQDZD8?t=1160 */}
-			<button
-				type="submit"
-				onClick={(e) =>
-					setFormList(formList.concat(newForm(name, desc)))
-				}
-			>
-				New form
-			</button>
-			<div>{JSON.stringify(formList, null, 2)}</div>
+			<div className="addNewList">
+				{" "}
+				<h1>PS forms</h1>
+				<input
+					onChange={(ev) => setName(ev.target.value)}
+					type="text"
+					placeholder="List Name"
+					id="name"
+					value={name}
+				/>
+				<input
+					onChange={(ev) => setDesc(ev.target.value)}
+					type="text"
+					placeholder="List Desciption"
+					id="description"
+					value={desc}
+				/>
+				{/* https://youtu.be/pCA4qpQDZD8?t=1160 */}
+				<button
+					type="submit"
+					onClick={(e) =>
+						setFormList(formList.concat(newForm(name, desc)))
+					}
+				>
+					New form
+				</button>
+				<div>{JSON.stringify(formList, null, 2)}</div>
+			</div>
+			<div className="list">
+				<ul>
+					{formList.map((e) => {
+						return (
+							<li>
+								Name:{e.name} Desc:{e.description}
+							</li>
+						);
+					})}
+
+					<li></li>
+				</ul>
+			</div>
 		</div>
 	);
 }
