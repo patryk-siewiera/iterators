6db2eef2cbfcab723a9f6a60a8b3fefb6396245d
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-16 13:03:41 +0200

    feat: name, desc into object

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function newForm(name) {
+function newForm(name, description) {
 	const form = {
 		name: name,
+		description: description,
 	};
 	return form;
 }
 
 function App() {
 	const [formList, setFormList] = useState([]);
+	const [name, setName] = useState("");
+	const [desc, setDesc] = useState("");
 	return (
 		<div className="App">
 			<h1>PS forms</h1>
+			<input
+				onChange={(ev) => setName(ev.target.value)}
+				type="text"
+				placeholder="List Name"
+				id="name"
+				value={name}
+			/>
+			<input
+				onChange={(ev) => setDesc(ev.target.value)}
+				type="text"
+				placeholder="List Desciption"
+				id="description"
+				value={desc}
+			/>
+			{/* https://youtu.be/pCA4qpQDZD8?t=1160 */}
 			<button
-				onClick={(e) => setFormList(formList.concat(newForm("list1")))}
+				type="submit"
+				onClick={(e) =>
+					setFormList(formList.concat(newForm(name, desc)))
+				}
 			>
 				New form
 			</button>
-			<div>{JSON.stringify(formList)}</div>
+			<div>{JSON.stringify(formList, null, 2)}</div>
 		</div>
 	);
 }
