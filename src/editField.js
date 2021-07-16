a4120473bf7c086058cdd92e420a88097f7022a5
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-16 15:47:27 +0200

    feat: type of questions

--- src/editField.js
+++ src/editField.js
@@ -10,7 +10,7 @@ export default function EditField() {
 				onChange={(ev) => setQuestionType(ev.target.value)}
 			>
 				<option value="shortAnswer">Short Answer</option>
-				<option value="long Answer">Long Answer</option>
+				<option value="longAnswer">Long Answer</option>
 				<option value="oneAnswer">One Answer</option>
 				<option value="multipleAnswer">Multiple Answer</option>
 				<option value="line">Line</option>
@@ -34,7 +34,62 @@ export default function EditField() {
 				Move down
 			</button>
 			<div className="question">
-				<input type="text" placeholder="question..." />
+				{questionType === "shortAnswer" && (
+					<input
+						className="shortAnswer"
+						type="text"
+						placeholder="question..."
+					/>
+				)}
+				{questionType === "longAnswer" && (
+					<textarea
+						className="longAnswer"
+						type="text"
+						placeholder="question..."
+						rows="5"
+					/>
+				)}
+				{questionType === "oneAnswer" && (
+					<div className="answers">
+						<label>
+							<input type="radio" name="answer" /> answer
+						</label>
+						<label>
+							<input type="radio" name="answer" /> answer
+						</label>
+						<label>
+							<input type="radio" name="answer" /> answer
+						</label>
+						<label>
+							<input type="radio" name="answer" /> answer
+						</label>
+						<button>add more </button>
+					</div>
+				)}
+				{questionType === "multipleAnswer" && (
+					<div className="answers">
+						<label>
+							<input type="checkbox" name="answer" /> answer
+						</label>
+						<label>
+							<input type="checkbox" name="answer" /> answer
+						</label>
+						<label>
+							<input type="checkbox" name="answer" /> answer
+						</label>
+						<label>
+							<input type="checkbox" name="answer" /> answer
+						</label>
+						<button>add more </button>
+					</div>
+				)}
+
+				{questionType === "line" && (
+					<div>
+						<input type="text" placeholder="Minimum value..." />
+						<input type="text" placeholder="Maximum value..." />
+					</div>
+				)}
 			</div>
 			{(questionType === "oneAnswer" ||
 				questionType === "multipleAnswer") && (
