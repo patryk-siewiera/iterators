27ccd57dce2a231b07794e337c927b57a61bfc23
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-21 13:43:48 +0200

    feat: draft react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,16 +3,39 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import EditForm from "./editForm";
-// import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import ViewForm from "./viewForm";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 ReactDOM.render(<MyApp />, document.getElementById("root"));
 
 function MyApp() {
-	const [page, setPage] = useState("edit");
+	const [page, setPage] = useState("ViewForm");
 	const [id, setId] = useState({});
 	return (
 		<React.StrictMode>
-			{page === "app" && (
+			<BrowserRouter>
+				<Route exact path="/">
+					<App
+						onEdit={(id) => {
+							setPage("edit");
+							setId(id);
+						}}
+					/>
+				</Route>
+				<Route exact path="/edit">
+					<EditForm
+						form={id}
+						onBack={() => {
+							setPage("app");
+						}}
+					/>
+				</Route>
+				<Route exact path="/view">
+					<ViewForm />
+				</Route>
+			</BrowserRouter>
+
+			{/* {page === "app" && (
 				<App
 					onEdit={(id) => {
 						setPage("edit");
@@ -29,6 +52,8 @@ function MyApp() {
 					}}
 				/>
 			)}
+
+			{page === "ViewForm" && <ViewForm />} */}
 		</React.StrictMode>
 	);
 }
---
+++ src/viewForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ViewForm() {
+	return <div>this is view form</div>;
+}
